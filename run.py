71a7f9d8f8533050a1cbcf1803f5506c96cacdbf
Nathan Nicholson <nathan.bytecode@gmail.com>
2023-07-26 08:24:20 +0000

    Prompt user input and test all answer outcomes

--- run.py
+++ run.py
@@ -2,3 +2,20 @@
 print("Welcome to Fable Quest, an adventure text based game.")
 print("To win, you must select the correct weapons to use.")
 print("Would you like to play? [yes/no]")
+
+### Prompt user input
+answer = input("> ")
+
+if(answer.lower().strip() == "yes"):
+    ### If "yes", prompt user for name
+   answer = input('Noble choice! What is your name hero?\n')
+elif(answer == "no"):
+    ### If "no", print goodbye message and quit program
+    print("Not all have what it takes to be a hero. Goodbye")
+    quit()
+else:
+    ### If neither "yes" or "no", print invalid answer message
+    print("Invalid answer. Try again running the program again.")
+    quit()
+
+### All outcomes have been tested to ensure user experience
