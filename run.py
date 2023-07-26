556816d64978352ec696a8bb722abb4fc2222483
Nathan Nicholson <nathan.bytecode@gmail.com>
2023-07-26 08:18:11 +0000

    Initial commit

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+### Welcome message
+print("Welcome to Fable Quest, an adventure text based game.")
+print("To win, you must select the correct weapons to use.")
+print("Would you like to play? [yes/no]")
