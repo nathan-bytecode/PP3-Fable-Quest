9c4bf6a636dc69430a4900373fc15d884d6066fd
Nathan Nicholson <nathan.bytecode@gmail.com>
2023-07-28 12:26:53 +0000

    Adjust some formatting

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-### Global varibales defined
+# Global varibales defined
 name = ""
 weapon = ""
 
-### Functions go on outer level to later be called upon
+
+# Functions go on outer level to later be called upon
 def training():
     if choice == "yes":
         weapon = ""
@@ -10,18 +11,20 @@ def training():
             try:
                 weapon = input("Choose weapon: [sword/bow/staff]\n")
                 weapon = weapon.lower().strip()
-        
                 if weapon == "sword":
                     print("Sword: effective aganist orcs who weild hammers.")
-                    weapon = input("Do you want to learn about another weapon? [yes/no]\n")
+                    print("Do you want to learn about another weapon?")
+                    weapon = input("[yes/no]\n")
                 elif weapon == "bow":
-                    print ("Bow: effective agansit bandits who weild daggers.")
-                    weapon = input("Do you want to learn about another weapon? [yes/no]\n")
+                    print("Bow: effective agansit bandits who weild daggers.")
+                    print("Do you want to learn about another weapon?")
+                    weapon = input("[yes/no]\n")
                 elif weapon == "staff":
                     print("Staff: effective agansit trolls who weild spears.")
-                    weapon = input("Do you want to learn about another weapon? [yes/no]\n")
+                    print("Do you want to learn about another weapon?")
+                    weapon = input("[yes/no]\n")
                 elif weapon == "no":
-                    print("I hope you've learned each weapon for what lies ahead!")
+                    print("I hope you've trained with each weapon!")
                     print("Get ready for battle!")
                     return
                 else:
@@ -33,12 +36,16 @@ def training():
         print("Get ready for battle!")
         return
 
+
 def stage_one():
     try:
-        weapon = input("You encounter a group of orcs, what weapon do you use? [sword, bow or staff]\n")
+        print("You encounter a group of orcs.")
+        print("What weapon do you use?")
+        weapon = input([sword, bow or staff]\n")
 
         if weapon == "sword":
-            print("Victory! A sword is effective agansit orcs! Move forward hero!\n")
+            print("Victory! A sword is effective agansit orcs!")
+            print("Move forward hero!")
         elif weapon == "bow":
             print("Game over. Orcs weild hammers. You get smashed.")
         elif weapon == "staff":
@@ -51,16 +58,20 @@ def stage_one():
         print("Invalid answer. Try again!")
         stage_one()
 
+
 def stage_two():
     try:
-        weapon = input("You encounter a group of trolls, what weapon do you use? [sword, bow or staff]\n")
+        print("You encounter a group of trolls.")
+        print("What weapon do you use?")
+        weapon = input([sword, bow or staff]\n")
 
         if weapon == "sword":
             print("Game over. Trolls weild spears. You get peirced.")
         elif weapon == "bow":
             print("Game over. Trolls weild spears. You get peirced.")
         elif weapon == "staff":
-            print("Victory! A staff is effective agansit trolls! Move forward hero!\n")
+            print("Victory! A staff is effective agansit trolls!")
+            print("Move forward hero!")
             return
         else:
             raise ValueError
@@ -69,9 +80,12 @@ def stage_two():
         print("Invalid answer. Try again!")
         stage_two()
 
+
 def stage_three():
     try:
-        weapon = input("You encounter a group of bandits, what weapon do you use? [sword, bow or staff]\n")
+        print("You encounter a group of bandits.")
+        print("What weapon do you use?")
+        weapon = input([sword, bow or staff]\n")
 
         if weapon == "sword":
             print("Game over. Bandits weild daggers. You get sliced.")
@@ -84,8 +98,9 @@ def stage_three():
             raise ValueError
 
     except ValueError:
-            print("Invalid answer. Try again!")
-            stage_three()
+        print("Invalid answer. Try again!")
+        stage_three()
+
 
 def crossroads():
     go = input("You come to a cross roads mid way up Forbidden Mountain, do you go back for more glory? Go right to rest at the Inn? Or go forward to more difficult battles? [back, right, forward]\n")
@@ -100,6 +115,7 @@ def crossroads():
     elif go =="forward":
         print("The difficulty has just gotten harder hero, multiple groups of enemies joined together are ahead. Choose the effective weapons for each attack wisely.\n")
         stage_four()
+
 
 def inn():
     try:
@@ -118,8 +134,9 @@ def inn():
             raise ValueError
 
     except ValueError:
-            print("Invalid answer. Try again!")
-            inn()
+        print("Invalid answer. Try again!")
+        inn()
+
 
 def stage_four():
     print("You encounter a group of bandits and trolls.")
@@ -151,8 +168,9 @@ def stage_four():
             raise ValueError
 
     except ValueError:
-            print("Invalid answer. Try again!")
-            stage_four()
+        print("Invalid answer. Try again!")
+        stage_four()
+
 
 def stage_five():
     print("You encounter a group of orcs and trolls.")
@@ -184,8 +202,9 @@ def stage_five():
             raise ValueError
 
     except ValueError:
-            print("Invalid answer. Try again!")
-            stage_five()
+        print("Invalid answer. Try again!")
+        stage_five()
+
 
 def stage_six():
     print("You encounter a group of bandits and orcs.")
@@ -217,8 +236,9 @@ def stage_six():
             raise ValueError
 
     except ValueError:
-            print("Invalid answer. Try again!")
-            stage_six()
+        print("Invalid answer. Try again!")
+        stage_six()
+
 
 def mid_story():
     print(f"Well done hero {answer}!")
@@ -229,6 +249,7 @@ def mid_story():
     print("With the Sword of the Ancients I can easily shapeshift my weapon to any of my choosing!")
     print("Prepare to taste my blade!")
     stage_seven()
+
 
 def stage_seven():
     print("Arbitar releases a relentless flurry of attacks! All your expeience has amounted to this battle! Choose wisely hero!")
@@ -251,6 +272,7 @@ def stage_seven():
         print("Invalid answer. Try again!")
         stage_seven()
 
+
 def stage_eight():
     try:
         weapon = input("Arbitar attacks in the form of a spear, what weapon do you use? [sword, bow or staff]\n")
@@ -271,6 +293,7 @@ def stage_eight():
         print("Invalid answer. Try again!")
         stage_eight()
 
+
 def stage_nine():
     try:
         weapon = input("Arbitar attacks in the form of a hammer, what weapon do you use? [sword, bow or staff]\n")
@@ -290,6 +313,7 @@ def stage_nine():
     except ValueError:
         print("Invalid answer. Try again!")
         stage_nine()
+
 
 def final_story():
     print("Arbitar: How can this be? A new champion has emerged! Have mercy on me champion of Albion!")
@@ -317,30 +341,30 @@ def final_story():
 
     
 
-### Welcome message
+# Welcome message
 print("Welcome to Fable Quest, an adventure text based game.")
 print("To win, you must select the correct weapons to use.")
 print("Would you like to play? [yes/no]")
 
-### Prompt user input
+# Prompt user input
 answer = input("> ")
 
 if(answer.lower().strip() == "yes"):
-    ### If "yes", prompt user for name
+    # If "yes", prompt user for name
    answer = input('Noble choice! What is your name hero?\n')
 elif(answer == "no"):
-    ### If "no", print goodbye message and quit program
+    # If "no", print goodbye message and quit program
     print("Not all have what it takes to be a hero. Goodbye")
     quit()
 else:
-    ### If neither "yes" or "no", print invalid answer message
+    # If neither "yes" or "no", print invalid answer message
     print("Invalid answer. Try again running the program again.")
     quit()
     ### All answer input outcomes have been tested to ensure user experience
 
 try:
     if answer == "":
-        ### If user gives an empty string, I have assigned a default name
+        # If user gives an empty string, I have assigned a default name
         raise ValueError
     else:
         name == answer
@@ -348,9 +372,9 @@ except ValueError:
     print("The mysterious type, ey? OK, I'll call you Flynn!")
     answer == 'Flynn'
 finally:
-    ### finally keyword exceutes the program regardless if there's an error or not
+    # finally keyword exceutes the program regardless if there's an error or not
 
-    ### Background story with the usage of an f-string for the user's name
+    # Background story with the usage of an f-string for the user's name
     print("Long ago in the land of Albion a hero once held in high honor")
     print("who desired more power and was drawn to the dark side.")
     print("This fallen hero known as Arbitar went to Forbidden Mountain")
